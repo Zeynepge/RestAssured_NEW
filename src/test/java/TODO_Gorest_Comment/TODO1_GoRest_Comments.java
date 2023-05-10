e3122156c3a448ff255d8a05f8cbe1a9c1f6c8fc
Zeynepge <sgunal571@gmail.com>
2023-05-10 15:14:08 +0200

    TODO Comment

--- src/test/java/TODO_Gorest_Comment/TODO1_GoRest_Comments.java
+++ src/test/java/TODO_Gorest_Comment/TODO1_GoRest_Comments.java
@@ -7,11 +7,16 @@ import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 
-/*Eğer bir data yo body de göndereceksek ve içerisinde hem int ve hemde value değerler var ise Map in tanımlama şekli aşağıdaki gibi olmalı Map<Stringi,Object>  @Student
+/*Eğer bir data yo body de göndereceksek ve içerisinde hem int ve hemde value değerler var ise Map in tanımlama şekli aşağıdaki gibi olmalı
+Map<Stringi,Object>  @Student
 Map<String,Object> newComment = new HashMap<>();
         newComment.put("post_id",16394);
         newComment.put("name",fullname);
@@ -21,10 +26,9 @@ Faker her zaman initialize edilmeli    Faker randomGenerator=new Faker();   gibi
 
  */
 
-
 public class TODO1_GoRest_Comments {
 
-    Faker randomfaker3=new Faker();
+    Faker faker2=new Faker();
     RequestSpecification reqSpec;
     int commentID;
 
@@ -34,20 +38,20 @@ public class TODO1_GoRest_Comments {
         baseURI="https://gorest.co.in/public/v2/comments";
 
         reqSpec=new RequestSpecBuilder()
-                .addHeader("Authorization","Bearer 3f4c352c7990cf988a5366a7baab6260362ca54a8bedc0a416b02e48926443b8")
+                .addHeader("Authorization","Bearer 6fdc14a1ab97bf9fecdaa233e63cfee45f2be7ab59d879668664b51639ba2de5")
                 .setContentType(ContentType.JSON)
                 .build();
     }
 
     @Test(enabled = true)
-    public void createUserClass(){
+    public void createCommentClass(){
 
         Comment newComment= new Comment();
 
-        newComment.post_id=randomfaker3.number().numberBetween(100,20000);
-        newComment.name=randomfaker3.name().fullName();
-        newComment.email=randomfaker3.internet().emailAddress();
-        newComment.body=randomfaker3.book().author();
+        newComment.post_id=19471;
+        newComment.name=faker2.name().fullName();
+        newComment.email=faker2.internet().emailAddress();
+        newComment.body=faker2.book().author();
 
         commentID=
                 given()
@@ -55,7 +59,6 @@ public class TODO1_GoRest_Comments {
                         .body(newComment)
 
                         .log().uri()
-                        .log().body()
 
                         .when()
                         .post("")
@@ -63,22 +66,21 @@ public class TODO1_GoRest_Comments {
                         .then()
                         .statusCode(201)
                         .contentType(ContentType.JSON)
+                        .log().body()
                         .extract().path("id")
         ;
 
     }
 
-   /* @Test(enabled = false)
-    public void createUserMap() {
-
-       // String rndFullname = randomUretici.name().fullName();
-        //String rndEmail = randomUretici.internet().emailAddress();
+    @Test(enabled = false)
+    public void createCommentMap() {
 
         Map<String,Object> newComment = new HashMap<>();
-        newComment.put("post_id",randomfaker2.number().numberBetween(100,20000));
-        newComment.put("name",randomfaker2.name().fullName());
-        newComment.put("email",randomfaker2.internet().emailAddress());
-        newComment.put("body",randomfaker2.book().author());
+
+        newComment.put("post_id",19471);
+        newComment.put("name",faker2.name().fullName());
+        newComment.put("email",faker2.internet().emailAddress());
+        newComment.put("body",faker2.book().author());
 
 
         commentID=
@@ -87,7 +89,6 @@ public class TODO1_GoRest_Comments {
                         .body(newComment)
 
                         .log().uri()
-                        .log().body()
 
                         .when()
                         .post("")
@@ -97,7 +98,90 @@ public class TODO1_GoRest_Comments {
                         .contentType(ContentType.JSON)
                         .extract().path("id")
         ;
-    }*/
+    }
+
+    @Test(dependsOnMethods = "createCommentClass")
+    public void getCommentByID(){
+
+        given()
+                .spec(reqSpec)
+
+                .when()
+                .get(""+commentID)
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("id",equalTo(commentID))
+        ;
+
+    }
+
+    @Test(dependsOnMethods = "getCommentByID")
+    public void updateCommentByID(){
+
+        Comment newComment= new Comment();
+        String name=faker2.name().fullName();
+        String email=faker2.internet().emailAddress();
+        String body_=faker2.book().author();
+
+        newComment.post_id=19471;
+        newComment.name=name;
+        newComment.email=email;
+        newComment.body=body_;
+
+        given()
+                .spec(reqSpec)
+
+                .body(newComment)
+
+                .when()
+                .put(""+commentID)
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("id",equalTo(commentID))
+                .body("name",equalTo(name))
+                .body("email",equalTo(email))
+                .body("body",equalTo(body_))
+        ;
+
+    }
+
+    @Test(dependsOnMethods = "updateCommentByID")
+    public void deleteCommentByID()
+    {
+        given()
+                .spec(reqSpec)
+
+                .when()
+                .delete(""+commentID)
+
+                .then()
+                .log().body()
+                .statusCode(204)
+
+        ;
+    }
+
+    @Test(dependsOnMethods = "deleteCommentByID")
+    public void deleteCommentByIDNegative(){
+        given()
+                .spec(reqSpec)
+
+                .when()
+                .delete(""+commentID)
+
+                .then()
+                .statusCode(404)
+        ;
+
+    }
+
+
 
 
 
